1f01fc01015751bc5f8d722e15de52d398c8bea1
Kyle Thompson <r7thompson@ucsd.edu>
2023-09-17 22:58:17 +0000

    Completed implementing a version of proof search. The search remains inefficient

--- test-coq-projs/example.v
+++ test-coq-projs/example.v
@@ -4,4 +4,4 @@ Ltac reduce_eq := simpl; reflexivity.
 
 Theorem mult_0_plus : ∀ n m : nat, 0 + (S n * m) = S n * m.
 Proof.
-Admitted.
+<prove>
---
+++ test-coq-projs/harder_example.v
@@ -0,0 +1,5 @@
+Require Import Nat.
+
+Lemma leb_refl: forall (n : nat), (n <=? n) = true.
+Proof.
+<prove>
