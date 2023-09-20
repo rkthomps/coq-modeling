5c2f265cd8a82e0766902726cbdd0c9b5122baf4
rkthomps <rkthomps@calpoly.edu>
2023-09-20 10:57:36 -0700

    Pretty much figured out how to compile all coq files

--- src/evaluation/compile_corpus.py
+++ src/evaluation/compile_corpus.py
@@ -2,37 +2,50 @@
 
 import sys, os
 import argparse
+import subprocess
 from impose_file_hierarchy import NEW_ROOT_NAME 
 
-COQ_PROJECT_NAME = "_CoqProject"
+PROJECT_NAME = "_CoqProject"
+MAKEFILE_NAME = "CoqMakefile"
+MAKEFILE_CONF_NAME = "CoqMakefile.conf"
+SPECIAL_FILES = [PROJECT_NAME, MAKEFILE_NAME, MAKEFILE_CONF_NAME]
 
 COQ_PROJECT_PREFIX = "-R theories CoqRepos"
 
-def get_project_dirs(tree_parent_loc: str) -> list[str]:
-    assert NEW_ROOT_NAME in os.listdir(tree_parent_loc)
-    repo_paths = []
-    root_loc = os.path.join(tree_parent_loc, NEW_ROOT_NAME)
-    for repo in os.listdir(root_loc):
-        repo_path = os.path.join(NEW_ROOT_NAME, repo)
-        repo_paths.append(repo_path)
-    return repo_paths
+def clean_directory(dir_loc: str) -> None:
+    parent_loc = os.path.dirname(dir_loc)
+    for s_fname in SPECIAL_FILES:
+        s_loc = os.path.join(parent_loc, s_fname)
+        if os.path.exists(s_loc):
+            os.remove(s_loc)
 
 
-def compile_corpus(tree_parent_loc: str) -> None:
-    coq_project_loc = os.path.join(tree_parent_loc, COQ_PROJECT_NAME)
-    if os.path.exists(coq_project_loc):
-        os.remove(coq_project_loc)
-    project_dirs = get_project_dirs(tree_parent_loc)
-    dirs_as_str = "\n".join(project_dirs)
-    coq_project_contents = f"{COQ_PROJECT_PREFIX}\n{dirs_as_str}"
+def get_coq_project_contents(dir_loc: str) -> str:
+    dir_name = os.path.basename(dir_loc)
+    return f"-R {dir_name} MyProject\n{dir_name}"
+
+
+def compile_directory(dir_loc: str) -> None:
+    clean_directory(dir_loc)
+    parent_dir = os.path.dirname(dir_loc)
+    os.chdir(parent_dir)
+    coq_project_loc = os.path.join(parent_dir, PROJECT_NAME)
     with open(coq_project_loc, "w") as fout:
-        fout.write(coq_project_contents)
+        fout.write(get_coq_project_contents(dir_loc))
+    p_create_make = subprocess.run(["coq_makefile", "-f", PROJECT_NAME, "-o", MAKEFILE_NAME])
+    assert p_create_make.returncode == 0
+    p_make = subprocess.run(["make", "-f", MAKEFILE_NAME])
+    if p_make.returncode != 0:
+        for subdir_name in os.listdir(dir_loc):
+            subdir_loc = os.path.join(dir_loc, subdir_name)
+            if os.path.isdir(subdir_loc):
+                compile_directory(subdir_loc)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Compile a set of coq repos.")
     parser.add_argument("tree_parent_loc", help="Location of the output directory from impose_file_hierarchy.py.")
     args = parser.parse_args(sys.argv[1:])
-    compile_corpus(args.tree_parent_loc)
-
-
+    compile_target = os.path.join(args.tree_parent_loc, NEW_ROOT_NAME)
+    compile_target_full_path = os.path.abspath(compile_target)
+    compile_directory(compile_target_full_path)
--- src/evaluation/impose_file_hierarchy.py
+++ src/evaluation/impose_file_hierarchy.py
@@ -7,9 +7,6 @@ import pdb
 import json
 from tqdm import tqdm
 
-# My friend for compiling coq: https://coq.inria.fr/refman/practical-tools/utilities.html
-# Generate make file:  coq_makefile -f _CoqProject -o CoqMakefile
-# Run make file: make -f CoqMakefile
 
 NEW_ROOT_NAME = "coq-repos"
 FILE_MAPPING_NAME = "mapping.json"
@@ -33,30 +30,8 @@ class FileTree:
         self.children.append(FileTree(path[1]))
         self.children[-1].add(path[1:])
 
-    def squeeze(self) -> None:
-        if len(self.children) == 1:
-            only_child = self.children[0] 
-            if only_child.value.endswith(".v"):
-                return
-            self.value = self.value + "-" + only_child.value 
-            self.children = only_child.children
-            self.squeeze()
-        for child in self.children:
-            child.squeeze()
-
-    def get_squeezed_path(self, file_tokens: list[str]) -> str:
-        assert len(file_tokens) > 0
-        assert file_tokens[0] == self.value
-        if file_tokens[0].endswith(".v"):
-            return file_tokens[0]
-        assert len(file_tokens) > 1
-        while True:
-            for c in self.children:
-                if file_tokens[1] == c.value:
-                    return os.path.join(self.value, c.get_squeezed_path(file_tokens[1:]))
-            first_token = file_tokens.pop(1)
-            assert len(file_tokens) > 1
-            file_tokens[1] = first_token + "-" + file_tokens[1]
+    def get_path(self, file_tokens: list[str]) -> str:
+        return "/".join(file_tokens)
 
 
 def prep_file_name(file_name: str) -> list[str]:
@@ -84,7 +59,6 @@ def create_heirarchy(data_loc: str, hierarchy_loc: str) -> None:
             continue
         dir_list = prep_file_name(orig_name) 
         ftree.add(dir_list)
-    ftree.squeeze()
 
     print("Copying files...")
     path_mappings: dict[str, str] = {}
@@ -92,10 +66,10 @@ def create_heirarchy(data_loc: str, hierarchy_loc: str) -> None:
         if not orig_name.endswith(".v"):
             continue
         dir_list = prep_file_name(orig_name)
-        squeezed_path = ftree.get_squeezed_path(dir_list)
-        full_squeezed_path = os.path.join(hierarchy_loc, squeezed_path) 
-        path_mappings[orig_name] = full_squeezed_path
-        squeezed_dirname = os.path.dirname(full_squeezed_path)
+        file_path = ftree.get_path(dir_list)
+        full_file_path = os.path.join(hierarchy_loc, file_path) 
+        path_mappings[orig_name] = full_file_path
+        squeezed_dirname = os.path.dirname(full_file_path)
         if not os.path.exists(squeezed_dirname):
             os.makedirs(squeezed_dirname)
         orig_coq_file_path = find_coq_file(os.path.join(data_loc, orig_name))
@@ -103,7 +77,7 @@ def create_heirarchy(data_loc: str, hierarchy_loc: str) -> None:
 
     print("Writing mapping...")
     with open(os.path.join(hierarchy_loc, FILE_MAPPING_NAME), "w") as fout:
-        fout.write(json.dumps(path_mappings))
+        fout.write(json.dumps(path_mappings, indent=2))
 
 
 if __name__ == "__main__":
