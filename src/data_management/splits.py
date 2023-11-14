39033f2e889e610e082e34f9869669e18689889d
Kyle Thompson <r7thompson@ucsd.edu>
2023-11-14 00:25:08 +0000

    Create stubs for partitioning dataset

--- src/data_management/splits.py
+++ src/data_management/splits.py
@@ -2,22 +2,37 @@
 Given a folder with a bunch of repositories, 
 create a project-wise split according to time. 
 """
+from __future__ import annotations
 
 import typeguard
 import argparse
 
 
+@typeguard.typechecked
 class DataSplit:
-    @typeguard.typechecked
     def __init__(
         self,
         train_projects: list[str],
         val_projects: list[str],
         test_projects: list[str],
+        time_sorted: bool,
     ) -> None:
         self.train_projects = train_projects
         self.val_projects = val_projects
         self.test_projects = test_projects
+        self.time_sorted = time_sorted
+
+    @classmethod
+    def __create_time_sorted(cls, data_loc: str) -> DataSplit:
+        pass
+
+    @classmethod
+    def __create_random(cls, data_loc: str) -> DataSplit:
+        pass
+
+    @classmethod
+    def create(cls, data_loc: str, time_sorted: bool) -> DataSplit:
+        pass
 
 
 if __name__ == "__main__":
