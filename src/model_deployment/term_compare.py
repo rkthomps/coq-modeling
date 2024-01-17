e47d986cb2a5dfc9505c7fabd57b73b96e3096ed
Kyle Thompson <r7thompson@ucsd.edu>
2024-01-17 23:37:45 +0000

    Progress to goal term parsing

--- src/model_deployment/term_compare.py
+++ src/model_deployment/term_compare.py
@@ -1,24 +1,41 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 from dataclasses import dataclass
 
+import time
+import random
+from tqdm import tqdm
+import sys
 import os
+import shutil
+import ipdb
+import argparse
 
 from coqpyt.coq.base_file import CoqFile, GoalAnswer
-from coqpyt.coq.lsp.structs import Goal
+from coqpyt.coq.lsp.structs import Goal, Hyp
+from coqpyt.coq.exceptions import InvalidStepException
 from data_management.splits import DataSplit, FileInfo
 
-from parsy import string, regex, whitespace, seq, forward_declaration, Parser, peek
+from parsy import (
+    string,
+    regex,
+    whitespace,
+    seq,
+    forward_declaration,
+    Parser,
+    peek,
+    ParseError,
+)
 
-from data_management.splits import FileInfo
-from util.util import get_fresh_path
+from data_management.splits import FileInfo, DataSplit, Split
+from util.util import get_fresh_path, get_basic_logger
 
+_logger = get_basic_logger(__name__)
 
 term_p = forward_declaration()
-expr_p_app = forward_declaration()
-expr_p_var = forward_declaration()
+term_p_var = forward_declaration()
 
-__raw_name_p = regex(r"\w+") << whitespace.optional()
+__raw_name_p = regex(r"(\w|\.|')+") << whitespace.optional()
 __reserved_p = (
     string("with")
     | string("end")
@@ -27,6 +44,7 @@ __reserved_p = (
     | string("forall")
     | string("exists")
     | string("match")
+    | string("fun")
 ).should_fail("reserved")
 __name_p = peek(__reserved_p) >> __raw_name_p
 
@@ -68,7 +86,7 @@ ConstrExpr = ConstructorApp | VarT
 __colon_p = string(":") << whitespace
 
 
-def combine_param_set(vars: list[str], ty: str) -> list[Param]:
+def combine_param_set(vars: list[str], ty: Term) -> list[Param]:
     return [Param(v, ty) for v in vars]
 
 
@@ -79,19 +97,19 @@ def join_lists(*args: list[Any]) -> list[Any]:
     return result_list
 
 
-__param_set_p_raw = seq(__name_p.at_least(1) << __colon_p, __name_p).combine(
+__param_set_p_raw = seq(__name_p.at_least(1) << __colon_p, term_p).combine(
     combine_param_set
 )
 __param_set_p_paren = parens(__param_set_p_raw)
 
-__multi_param_set_p_paran = __param_set_p_paren.at_least(1).combine(join_lists)
-params_p = __param_set_p_raw | __multi_param_set_p_paran
+__multi_param_set_p_paren = __param_set_p_paren.at_least(1).combine(join_lists)
+params_p = __param_set_p_raw | __multi_param_set_p_paren
 
 
 @dataclass
 class Param:
     id: str
-    ty: str
+    ty: Term
 
 
 def combine_fix(id: str, params: list[Param], ty: str, body: Term) -> FixT:
@@ -105,9 +123,9 @@ __struct_arg_p = (
 __def_p = string(":=") << whitespace
 __fix_p_raw = seq(
     string("fix") << whitespace >> __name_p,
-    params_p << __struct_arg_p << __colon_p,
-    __name_p,
-    __def_p >> expr_p_app,
+    params_p << __struct_arg_p.optional() << __colon_p,
+    term_p,
+    __def_p >> term_p,
 ).combine(combine_fix)
 
 fix_p = __fix_p_raw | parens(__fix_p_raw)
@@ -117,7 +135,7 @@ fix_p = __fix_p_raw | parens(__fix_p_raw)
 class FixT:
     id: str
     params: list[Param]
-    ty: str
+    ty: Term
     body: Term
 
 
@@ -144,23 +162,34 @@ class ProdT:
 #         self.body = body
 
 
-# class FunT:
-#     def __init__(self, params: list[Param], body: Term) -> None:
-#         self.params = params
-#         self.body = body
+def combine_fun(params: list[Param], body: Term) -> FunT:
+    return FunT(params, body)
 
 
-abstraction_p = fix_p | var_p
+__fun_p_raw = seq(
+    string("fun") << whitespace >> params_p, string("=>") << whitespace >> term_p
+).combine(combine_fun)
+
+fun_p = __fun_p_raw | parens(__fun_p_raw)
 
 
-Abstraction = FixT | VarT
+@dataclass
+class FunT:
+    params: list[Param]
+    body: Term
 
 
-def combine_app_p(abstr: Abstraction, args: list[Expr]) -> AppT:
+abstraction_p = fix_p | var_p | fun_p
+
+
+Abstraction = FixT | VarT | FunT
+
+
+def combine_app_p(abstr: Abstraction, args: list[Term]) -> AppT:
     return AppT(abstr, args)
 
 
-__app_p_raw = seq(abstraction_p, expr_p_var.at_least(1)).combine(combine_app_p)
+__app_p_raw = seq(abstraction_p, term_p_var.at_least(1)).combine(combine_app_p)
 
 app_p = __app_p_raw | parens(__app_p_raw)
 
@@ -168,30 +197,30 @@ app_p = __app_p_raw | parens(__app_p_raw)
 @dataclass
 class AppT:
     fn: Abstraction
-    args: list[Expr]
+    args: list[Term]
 
 
-def combine_match_branch(pattern: ConstrExpr, body: Expr) -> MatchBranch:
+def combine_match_branch(pattern: ConstrExpr, body: Term) -> MatchBranch:
     return MatchBranch(pattern, body)
 
 
 match_branch_p = seq(
     string("|") << whitespace >> constr_expr_p << string("=>") << whitespace,
-    expr_p_app << whitespace.optional(),
+    term_p << whitespace.optional(),
 ).combine(combine_match_branch)
 
 
 @dataclass
 class MatchBranch:
     pattern: ConstrExpr
-    body: Expr
+    body: Term
 
 
-def combine_match(expr: Expr, branches: list[MatchBranch]) -> MatchT:
+def combine_match(expr: Term, branches: list[MatchBranch]) -> MatchT:
     return MatchT(expr, branches)
 
 
-__parens_expr_or_var_p = var_p | parens(expr_p_app)
+__parens_expr_or_var_p = var_p | parens(term_p)
 
 __match_p_raw = seq(
     string("match")
@@ -207,16 +236,17 @@ match_p = __match_p_raw | parens(__match_p_raw)
 
 @dataclass
 class MatchT:
-    expr: Expr
+    expr: Term
     branches: list[MatchBranch]
 
 
-Expr = FixT | MatchT | AppT | VarT
-Term = FixT | MatchT | AppT | VarT | ProdT
+Term = FixT | MatchT | AppT | VarT | ProdT | FunT
 
-term_p.become(app_p | fix_p | match_p | prod_p | var_p)
-expr_p_app.become(app_p | fix_p | match_p | var_p)
-expr_p_var.become(var_p | app_p | fix_p | match_p | var_p)
+__term_p_raw = app_p | fix_p | fun_p | match_p | prod_p | var_p
+term_p.become(__term_p_raw | parens(__term_p_raw))
+
+__term_p_var = var_p | app_p | fix_p | fun_p | match_p | prod_p
+term_p_var.become(__term_p_var | parens(__term_p_var))
 
 
 class EmptyFgGoalError(Exception):
@@ -236,18 +266,16 @@ def __appears_in_hyp_or_goal(var: str, goal: Goal) -> bool:
     return False
 
 
-def has_hyp(goal: Goal) -> bool:
-    return len(goal.hyps) > 0
-
-
-def get_generalize_var(goal: Goal) -> str:
-    if len(goal.hyps) == 0:
-        raise HypsEmptyError("No vars to generalize.")
+def get_generalize_var(goal: Goal, cannot_generalize: set[str]) -> Optional[str]:
+    default_candidate: Optional[str] = None
     for hyp in goal.hyps:
         for name in hyp.names:
+            if name in cannot_generalize:
+                continue
             if __appears_in_hyp_or_goal(name, goal):
                 return name
-    return goal.hyps[0].names[0]
+            default_candidate = name
+    return default_candidate
 
 
 def get_fg_goal(goal: GoalAnswer) -> Goal:
@@ -258,35 +286,105 @@ def get_fg_goal(goal: GoalAnswer) -> Goal:
     return fg_goals[0]
 
 
-def get_norm_goal(data_loc: str, file_info: FileInfo, proof_prefix: str) -> Term:
-    file_loc = os.path.join(data_loc, file_info.file)
-    workspace_loc = os.path.join(data_loc, file_info.workspace)
-    aux_file = get_fresh_path(".", "goal_aux.v")
+def __restore_coq_file(coq_file: CoqFile, end_idx: int, num_steps_added: int) -> None:
+    for i in range(1, num_steps_added + 1):
+        added_step_idx = num_steps_added - i
+        delete_idx = end_idx + added_step_idx
+        coq_file.delete_step(delete_idx)
 
+
+def __exec_to(coq_file: CoqFile, idx: int) -> None:
+    while coq_file.steps_taken < idx:
+        coq_file.exec()
+
+
+def get_norm_goal(coq_file: CoqFile, end: int) -> Term:
+    steps_added = 0
     try:
-        with open(aux_file, "w") as fout:
-            fout.write(proof_prefix)
-        with CoqFile(file_loc, workspace_loc) as coq_file:
-            coq_file.run()
-            goals = coq_file.current_goals
-            fg_goal = get_fg_goal(goals)
-            while has_hyp(fg_goal):
-                generalize_var = get_generalize_var(fg_goal)
-                last_step = len(coq_file.steps)
-                step_text = f"\ngeneralize dependent {generalize_var}."
-                coq_file.add_step(last_step - 1, step_text)
+        __exec_to(coq_file, end)
+        goals = coq_file.current_goals
+        fg_goal = get_fg_goal(goals)
+        cannot_generalize: set[str] = set()
+        generalize_var = get_generalize_var(fg_goal, cannot_generalize)
+        while generalize_var:
+            step_text = f"\ngeneralize dependent {generalize_var}."
+            try:
+                coq_file.add_step(end + steps_added - 1, step_text)
                 coq_file.exec()
+                steps_added += 1
                 goals = coq_file.current_goals
                 fg_goal = get_fg_goal(goals)
-            last_step = len(coq_file.steps)
-            rid_notations = "\nUnset Printing Notations."
-            coq_file.add_step(last_step - 1, rid_notations)
-            simpl = "\ncbv."
-            last_step = len(coq_file.steps)
-            coq_file.add_step(last_step - 1, simpl)
-            goals = coq_file.current_goals
-            fg_goal = get_fg_goal(goals)
-            return term_p.parse(fg_goal.ty)
+            except InvalidStepException:
+                cannot_generalize.add(generalize_var)
+            generalize_var = get_generalize_var(fg_goal, cannot_generalize)
+        rid_notations = "\nUnset Printing Notations."
+        coq_file.add_step(end + steps_added - 1, rid_notations)
+        coq_file.exec()
+        steps_added += 1
+        simpl = "\ncbv."
+        coq_file.add_step(end + steps_added - 1, simpl)
+        coq_file.exec()
+        steps_added += 1
+        goals = coq_file.current_goals
+        fg_goal = get_fg_goal(goals)
+        try:
+            parsed_term = term_p.parse(fg_goal.ty)
+            print("got it!")
+            return parsed_term
+        except ParseError:
+            print(len(fg_goal.ty))
+            if len(fg_goal.ty) < 400:
+                print(fg_goal.ty)
     finally:
-        if os.path.exists(aux_file):
-            os.remove(aux_file)
+        __restore_coq_file(coq_file, end, steps_added)
+
+
+def __clear_coq_file(coq_file: CoqFile) -> list[str]:
+    step_bank: list[str] = []
+    while len(coq_file.steps) > 0:
+        step_bank.insert(0, coq_file.steps[-1].text)
+        coq_file.delete_step(len(coq_file.steps) - 1)
+    return step_bank
+
+
+def mine_norm_goals(data_loc: str, data_split: DataSplit) -> None:
+    file_list = data_split.get_file_list(data_loc, Split.TRAIN)
+    random.seed(2)
+    random.shuffle(file_list)
+    temp_loc = get_fresh_path(".", "goal_aux.v")
+    for file_info in file_list:
+        print("Processing", file_info.file)
+        file_loc = os.path.join(data_loc, file_info.file)
+        workspace_loc = os.path.join(data_loc, file_info.workspace)
+        with CoqFile(file_loc, workspace=workspace_loc) as coq_file:
+            steps = [s.text for s in coq_file.steps]
+        shutil.copy(file_loc, temp_loc)
+        with CoqFile(temp_loc, workspace=workspace_loc) as coq_file:
+            if not coq_file.is_valid:
+                _logger.warning(f"{file_info.file} not valid initially. Skipping.")
+                continue
+
+            removed_steps = __clear_coq_file(coq_file)
+            for i, step in enumerate(removed_steps):
+                coq_file.add_step(i - 1, step)
+                __exec_to(coq_file, i + 1)
+                if coq_file.in_proof:
+                    if (
+                        coq_file.current_goals is None
+                        or coq_file.current_goals.goals is None
+                        or len(coq_file.current_goals.goals.goals) == 0
+                    ):
+                        continue
+                    get_norm_goal(coq_file, i + 1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Parser to make a database of proof states."
+    )
+    parser.add_argument("data_split_loc", help="Path to saved data split.")
+    parser.add_argument("data_loc", help="Path to coq dataset.")
+    args = parser.parse_args(sys.argv[1:])
+
+    data_split = DataSplit.load(args.data_split_loc)
+    mine_norm_goals(args.data_loc, data_split)
--- test/test_model_deployment/test_term_compare.py
+++ test/test_model_deployment/test_term_compare.py
@@ -1,4 +1,13 @@
-from model_deployment.term_compare import app_p, AppT, VarT
+from model_deployment.term_compare import (
+    term_p,
+    app_p,
+    AppT,
+    VarT,
+    ProdT,
+    Param,
+    FunT,
+    FixT,
+)
 
 
 class TestTermCompare:
@@ -9,3 +18,43 @@ class TestTermCompare:
         str2 = "a b c"
         res2 = app_p.parse(str2)
         assert res2 == AppT(VarT("a"), [VarT("b"), VarT("c")])
+
+    def test_failure_1(self) -> None:
+        test_str = "forall _ : (eq 1 3), False"
+        test_res = ProdT(
+            [Param("_", AppT(VarT("eq"), [VarT("1"), VarT("3")]))], VarT("False")
+        )
+        assert term_p.parse(test_str) == test_res
+
+    def test_failure_2(self) -> None:
+        # test_str = "forall (a : A) (s : forall _ : A, bool), sumbool (eq (s a) true) (forall _ : eq (s a) true, False)"
+        test_str = "a 1 (forall _ : True, False)"
+        test_res = AppT(
+            VarT("a"), [VarT("1"), ProdT([Param("_", VarT("True"))], VarT("False"))]
+        )
+        assert term_p.parse(test_str) == test_res
+
+    def test_failure_3(self) -> None:
+        # test_str = "forall (x : X) (f : forall _ : X, X) (_ : forall (x0 y : X) (_ : le x0 y), le (f x0) (f y)), le (f x) (lfp (fun Y : X => join (f Y) x))"
+        test_str = "fun Y : X => join (f Y) x"
+        test_res = FunT(
+            [Param("Y", VarT("X"))],
+            AppT(VarT("join"), [AppT(VarT("f"), [VarT("Y")]), VarT("x")]),
+        )
+        res_act = term_p.parse(test_str)
+        assert test_res == res_act
+
+    def test_failure_4(self) -> None:
+        test_str = "App.le"
+        test_res = VarT("App.le")
+        assert test_res == term_p.parse(test_str)
+
+    def test_failure_5(self) -> None:
+        test_str = "y'"
+        test_res = VarT("y'")
+        assert test_res == term_p.parse(test_str)
+
+    def test_failure_6(self) -> None:
+        test_str = "fix cat (a : nat) : nat := 1"
+        test_res = FixT("cat", [Param("a", VarT("nat"))], VarT("nat"), VarT("1"))
+        assert test_res == term_p.parse(test_str)
