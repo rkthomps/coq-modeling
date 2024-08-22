c740ff5b5aa560dde28ad583d7c04ce435851b74
Kyle Thompson <r7thompson@ucsd.edu>
2024-08-22 11:09:15 -0700

    Add lemma example code and a test case

---
+++ test/test_lemma_gen/test_lemma_example.py
@@ -0,0 +1,12 @@
+class TestLemmaExample:
+
+    def test_lemma_example(self):
+        pass
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
