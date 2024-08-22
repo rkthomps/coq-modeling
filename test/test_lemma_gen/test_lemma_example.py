677c263a069ccfd0f39017a1f7843794d45d9278
Kyle Thompson <r7thompson@ucsd.edu>
2024-08-22 11:24:49 -0700

    Working on test case

--- test/test_lemma_gen/test_lemma_example.py
+++ test/test_lemma_gen/test_lemma_example.py
@@ -1,3 +1,15 @@
+import shutil
+from util.test_utils import (
+    TEST_PROJET_LOC,
+    LIST_DEFS_LOC,
+    LIST_THMS_LOC,
+    TEST_TMP_LOC,
+    get_test_sentence_db,
+)
+
+from data_management.create_file_data_point import get_data_point, get_switch_loc
+
+
 class TestLemmaExample:
 
     def test_lemma_example(self):
@@ -5,8 +17,14 @@ class TestLemmaExample:
 
     @classmethod
     def setup_class(cls):
-        pass
+        cls.sentence_db = get_test_sentence_db()
+        cls.defs_dp = get_data_point(
+            LIST_DEFS_LOC, TEST_PROJET_LOC, cls.sentence_db, True, get_switch_loc()
+        )
+        cls.thms_dp = get_data_point(
+            LIST_THMS_LOC, TEST_PROJET_LOC, cls.sentence_db, True, get_switch_loc()
+        )
 
     @classmethod
     def teardown_class(cls):
-        pass
+        shutil.rmtree(TEST_TMP_LOC)
