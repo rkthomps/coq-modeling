0cf79f87206319a5c2c504f4c1cb498142efe99e
Kyle Thompson <r7thompson@ucsd.edu>
2024-08-22 17:09:42 -0700

    Add tests for lm example

--- test/test_tactic_gen/test_lm_example.py
+++ test/test_tactic_gen/test_lm_example.py
@@ -1,12 +1,11 @@
 import os
 import shutil
 import ipdb
-from util.test_utils import (
-    TEST_PROJET_LOC,
-    LIST_DEFS_LOC,
-    LIST_THMS_LOC,
-    TEST_TMP_LOC,
-    get_test_sentence_db,
+from test_utils.utils import (
+    LIST_DEFS_DP,
+    LIST_THMS_DP,
+    TEST_MINI_DATASET_LOC,
+    TEST_MINI_DATASET_SENTENCE_DB,
 )
 
 from premise_selection.premise_filter import (
@@ -17,7 +16,6 @@ from premise_selection.premise_filter import (
 from data_management.sentence_db import SentenceDB
 from data_management.splits import DATA_POINTS_NAME, REPOS_NAME
 from data_management.dataset_file import DatasetFile
-from data_management.create_file_data_point import get_data_point, get_switch_loc
 from tactic_gen.lm_example import (
     LmExample,
     GeneralFormatterConf,
@@ -25,6 +23,8 @@ from tactic_gen.lm_example import (
     formatter_conf_from_yaml,
     formatter_from_conf,
 )
+
+from test_utils.test_data_utils import setup_project_dataset
 
 
 class TestLmExample:
@@ -54,14 +54,14 @@ class TestLmExample:
                 "context_format_alias": "basic",
                 "premise_format_alias": "basic",
                 "premise_filter": {"known_filter": "proj-thm"},
-                "sentence_db_loc": self.sentence_db_loc,
+                "sentence_db_loc": TEST_MINI_DATASET_SENTENCE_DB,
             },
             "proof_ret": {
                 "alias": "sparse",
                 "kind": "tfidf",
                 "max_examples": 20,
-                "data_loc": self.test_data_loc,
-                "sentence_db_loc": self.sentence_db_loc,
+                "data_loc": TEST_MINI_DATASET_LOC,
+                "sentence_db_loc": TEST_MINI_DATASET_SENTENCE_DB,
             },
             "num_premises": 50,
             "num_proofs": 20,
@@ -73,12 +73,10 @@ class TestLmExample:
                 for step_idx, step in enumerate(proof.steps):
                     example = lm_formatter.example_from_step(step_idx, proof_idx, dp)
                     assert example.proofs is not None
-                    try:
-                        assert proof_idx <= len(example.proofs)
-                    except AssertionError:
-                        ipdb.set_trace()
                     assert example.premises is not None
-                    assert proof_idx <= len(example.premises)
+                    if 0 < len(step.goals):
+                        assert proof_idx <= len(example.proofs)
+                        assert proof_idx <= len(example.premises)
                     assert all(
                         proof.proof_text_to_string() not in p for p in example.proofs
                     )
@@ -89,42 +87,18 @@ class TestLmExample:
 
     @classmethod
     def setup_class(cls):
-        cls.test_data_loc = TEST_TMP_LOC / "test-data"
-        if cls.test_data_loc.exists():
-            shutil.rmtree(cls.test_data_loc)
-
-        os.makedirs(cls.test_data_loc / REPOS_NAME)
-        os.makedirs(cls.test_data_loc / DATA_POINTS_NAME)
-        cls.sentence_db_loc = cls.test_data_loc / "sentences.db"
-        cls.sentence_db = SentenceDB.create(cls.sentence_db_loc)
-        workspace_loc = cls.test_data_loc / REPOS_NAME / TEST_PROJET_LOC.name
-        shutil.copytree(TEST_PROJET_LOC, workspace_loc)
-
-        list_defs_loc = workspace_loc / (LIST_DEFS_LOC.relative_to(TEST_PROJET_LOC))
-        list_thms_loc = workspace_loc / (LIST_DEFS_LOC.relative_to(TEST_PROJET_LOC))
-
-        cls.defs_dp = get_data_point(
-            list_defs_loc, workspace_loc, cls.sentence_db, True, get_switch_loc()
+        setup_project_dataset()
+        cls.sentence_db = SentenceDB.load(TEST_MINI_DATASET_SENTENCE_DB)
+        cls.defs_dp = DatasetFile.load(
+            TEST_MINI_DATASET_LOC / DATA_POINTS_NAME / LIST_DEFS_DP, cls.sentence_db
         )
-        cls.thms_dp = get_data_point(
-            list_thms_loc, workspace_loc, cls.sentence_db, True, get_switch_loc()
-        )
-
-        cls.defs_dp.save(
-            cls.test_data_loc / DATA_POINTS_NAME / cls.defs_dp.dp_name,
-            cls.sentence_db,
-            insert_allowed=True,
-        )
-        cls.thms_dp.save(
-            cls.test_data_loc / DATA_POINTS_NAME / cls.thms_dp.dp_name,
-            cls.sentence_db,
-            insert_allowed=True,
+        cls.thms_dp = DatasetFile.load(
+            TEST_MINI_DATASET_LOC / DATA_POINTS_NAME / LIST_THMS_DP, cls.sentence_db
         )
 
     @classmethod
     def teardown_class(cls):
         cls.sentence_db.close()
-        shutil.rmtree(TEST_TMP_LOC)
 
 
 if __name__ == "__main__":
