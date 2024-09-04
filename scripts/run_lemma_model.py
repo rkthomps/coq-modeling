05adf3c4f66c7b701178b9ccb1f65a39560a8188
kyle <r7thompson@ucsd.edu>
2024-09-04 20:07:07 +0000

    Add lemma model script

---
+++ scripts/run_lemma_model.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+import yaml
+import ipdb
+from pathlib import Path
+
+from lemma_gen.lemma_example import LemmaFormatter, LemmaFormatterConf
+from lemma_gen.lemma_model_wrapper import LemmaDecoderWrapper
+from data_management.dataset_file import DatasetFile
+from data_management.dataset_utils import LemmaDatasetConf
+from data_management.sentence_db import SentenceDB
+from data_management.create_file_data_point import get_data_point
+
+from util.util import set_rango_logger, get_fresh_path
+from util.constants import RANGO_LOGGER, LEMMA_DATA_CONF_NAME
+
+import logging
+
+_logger = logging.getLogger(RANGO_LOGGER)
+
+
+DATA_POINTS_LOC = Path("raw-data/coq-dataset/data_points")
+
+
+def get_lemma_formatter_conf(checkpoint_loc: Path) -> LemmaFormatterConf:
+    data_conf_loc = checkpoint_loc.parent / LEMMA_DATA_CONF_NAME
+    with data_conf_loc.open("r") as fin:
+        yaml_conf = yaml.safe_load(fin)
+    dataset_conf = LemmaDatasetConf.from_yaml(yaml_conf)
+    return dataset_conf.lemma_formatter_conf
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("checkpoint_loc", help="Location of model checkpoint.")
+    parser.add_argument("file_loc", help="Location of Coq File.")
+    parser.add_argument("workspace_loc", help="Location of workspace.")
+
+    args = parser.parse_args()
+
+    set_rango_logger(__file__, logging.DEBUG)
+
+    checkpoint_loc = Path(args.checkpoint_loc)
+    file_loc = Path(args.file_loc)
+    workspace_loc = Path(args.workspace_loc)
+
+    _logger.info(f"Loading model from {checkpoint_loc}")
+    model = LemmaDecoderWrapper.from_checkpoint(checkpoint_loc)
+
+    _logger.info("Getting lemma formatter.")
+    lemma_formatter_conf = get_lemma_formatter_conf(checkpoint_loc)
+    lemma_formatter = LemmaFormatter.from_conf(lemma_formatter_conf)
+    _logger.warning("Have not implemented server for lemma generation.")
+
+    sentence_db_loc = get_fresh_path(Path("/tmp"), "sentence_db")
+    sentence_db = SentenceDB.create(sentence_db_loc)
+    try:
+        _logger.info("Getting data point.")
+        dp = get_data_point(file_loc, workspace_loc, sentence_db, False, None)
+
+        for i, proof in enumerate(dp.proofs):
+            for j, step in enumerate(proof.steps):
+                examples = lemma_formatter.examples_from_step(j, proof, dp)
+                for example in examples:
+                    lemma = model.get_lemma(example)
+                    print("==============================")
+                    if example.relevant_lemmas is not None:
+                        print("------ Lemmas in Context -----")
+                        print("\n".join(example.relevant_lemmas))
+                    print("------ State -------")
+                    print(example.current_state)
+                    print("------ Script -------")
+                    print(example.current_script)
+                    print("------ Generated Lemma ------")
+                    print(lemma)
+                    print("------ Target ------")
+                    print(example.target)
+                    print("==============================\n")
+
+    finally:
+        sentence_db.close()
+        os.remove(sentence_db_loc)
